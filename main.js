2403244144982e66848a050c0eecf55a6dc100bb
47419119l <47419119l@iespoblenou.org>
2016-04-14 17:03:12 +0200

    Millora codi

--- main.js
+++ main.js
@@ -40,11 +40,102 @@ var mainState = (function (_super) {
         this.game.load.image('basura', 'assets/basura.png');
         this.game.physics.startSystem(Phaser.Physics.ARCADE);
     };
+    /**
+     * Metodes que utilitzaré al create.
+     */
+    /**
+     * Metode configObjectes serveix per congigurar el group d'objectes.
+     */
     mainState.prototype.configObjectes = function () {
         this.objectes = this.add.group();
         this.objectes.enableBody = true;
         this.objectes.physicsBodyType = Phaser.Physics.ARCADE;
     };
+    /**
+     * Metode configGat serveix per configurar el Sprite Gat
+     */
+    mainState.prototype.configGat = function () {
+        this.gat = this.game.add.sprite(this.game.world.centerX, this.game.world.height - 83, 'gat');
+        // Cambiamos el "anchor" del jugador
+        this.gat.anchor.setTo(0.5, 0.5);
+        //Afegueixo fisica al gat.
+        this.game.physics.arcade.enable(this.gat);
+        // que colisione contra las paredes.
+        this.gat.body.collideWorldBounds = true;
+        // Agregamos gravedad al jugador
+        this.gat.body.gravity.y = 500;
+    };
+    /**
+     * Metode configWorld serveix per configurar el mon
+     */
+    mainState.prototype.configWorld = function () {
+        this.game.stage.backgroundColor = "#74538f";
+        this.terraesquerra = this.game.add.sprite(this.game.world.width - 798, this.game.world.height - 100, 'terraesquerra');
+        this.game.physics.arcade.enable(this.terraesquerra);
+        this.terraesquerra.body.immovable = true;
+        this.baldainici = this.game.add.sprite(this.game.world.width - 565, this.game.world.height - 35, 'baldainici');
+        this.game.physics.arcade.enable(this.baldainici);
+        this.baldainici.body.immovable = true;
+        this.terradreta = this.game.add.sprite(this.game.world.width - 250, this.game.world.height - 100, 'terradreta');
+        this.game.physics.arcade.enable(this.terradreta);
+        this.terradreta.body.immovable = true;
+        this.tiempo = this.game.add.text(20, 10, "Tiempo : " + this.CONTADORTIEMPO, {
+            font: "25px Fixedsys",
+            fill: "#fff",
+            align: "center"
+        });
+        this.game.time.events.loop(Phaser.Timer.SECOND, this.temporitzadorPartida, this);
+        this.game.time.events.loop(Phaser.Timer.SECOND, this.tempsObjectes, this);
+        // Cogemos los cursores para gestionar la entrada
+        this.cursor = this.game.input.keyboard.createCursorKeys();
+    };
+    /**
+     * Contador de la partida.
+     */
+    mainState.prototype.temporitzadorPartida = function () {
+        this.CONTADORTIEMPO--;
+        this.tiempo.setText("Tiempo : " + this.CONTADORTIEMPO);
+    };
+    /**
+     * Metode configbaldes serveix per col·locar les baldes a la posició que l'hi toca
+     */
+    mainState.prototype.configbaldes = function () {
+        this.baldes = this.add.group();
+        this.baldes.enableBody = true;
+        this.baldes.physicsBodyType = Phaser.Physics.ARCADE;
+        for (var x = 0; x < 4; x++) {
+            for (var y = 0; y < 5; y++) {
+                if (x == 1 && y == 2 || x == 2 && y == 0 || x == 2 && y == 4 || x == 3 && y == 1 || x == 3 && y == 2 || x == 3 && y == 3) {
+                    var newElement = new Balda(this.game, y * this.ESPAIH, x * this.ESPAIV + 50, 'baldes', 0);
+                    this.baldes.add(newElement);
+                }
+            }
+        }
+    };
+    mainState.prototype.create = function () {
+        _super.prototype.create.call(this);
+        this.configWorld();
+        this.configbaldes();
+        this.configGat();
+        this.configObjectes();
+    };
+    /**
+     * Metodes utilitzats al update
+     */
+    /**
+     * Metode que al chocar amb un obecte desapareix.
+     * @param gat - Sprite gat (jugador)
+     * @param objecte - Objectes que hi ha a les baldes
+     */
+    mainState.prototype.tirarObjectes = function (gat, objecte) {
+        var num = objecte.balda;
+        objecte.kill();
+        this.alliberarBlada(num);
+    };
+    /**
+     *Metode que retorna un tipus de objecte al atzar.
+     * @returns string tipus d'objecte
+     */
     mainState.prototype.tipusObjecte = function () {
         var tipus = Math.floor((Math.random() * 6) + 1);
         var object;
@@ -65,37 +156,9 @@ var mainState = (function (_super) {
         }
         return object;
     };
-    mainState.prototype.comprovarBaldes = function (balda) {
-        var resposta = false;
-        if (balda == 1 && !this.balda1) {
-            this.balda1 = true;
-        }
-        else if (balda == 2 && !this.balda2) {
-            this.balda2 = true;
-        }
-        else if (balda == 3 && this.balda3) {
-            this.balda3 = true;
-        }
-        else if (balda == 4 && this.balda4) {
-            this.balda4 = true;
-        }
-        else if (balda == 5 && this.balda5) {
-            this.balda5 = true;
-        }
-        else if (balda == 6 && this.balda6) {
-            this.balda4 = true;
-        }
-        else if (balda == 7 && this.balda7) {
-            this.balda7 = true;
-        }
-        else if (balda == 8 && this.balda8) {
-            this.balda8 = true;
-        }
-        else {
-            resposta = true;
-        }
-        return resposta;
-    };
+    /**
+     * Metode que crea objectes al atzar en una posició random
+     */
     mainState.prototype.crearObjectes = function () {
         var posiciones = [
             new Point(1, 2), new Point(2, 0),
@@ -149,79 +212,46 @@ var mainState = (function (_super) {
             this.objectes.add(newElement);
         }
     };
-    mainState.prototype.configGat = function () {
-        this.gat = this.game.add.sprite(this.game.world.centerX, this.game.world.height - 83, 'gat');
-        // Cambiamos el "anchor" del jugador
-        this.gat.anchor.setTo(0.5, 0.5);
-        //Afegueixo fisica al gat.
-        this.game.physics.arcade.enable(this.gat);
-        // que colisione contra las paredes.
-        this.gat.body.collideWorldBounds = true;
-        // Agregamos gravedad al jugador
-        this.gat.body.gravity.y = 500;
-    };
-    mainState.prototype.configWorld = function () {
-        this.terraesquerra = this.game.add.sprite(this.game.world.width - 798, this.game.world.height - 100, 'terraesquerra');
-        this.game.physics.arcade.enable(this.terraesquerra);
-        this.terraesquerra.body.immovable = true;
-        this.baldainici = this.game.add.sprite(this.game.world.width - 565, this.game.world.height - 35, 'baldainici');
-        this.game.physics.arcade.enable(this.baldainici);
-        this.baldainici.body.immovable = true;
-        this.terradreta = this.game.add.sprite(this.game.world.width - 250, this.game.world.height - 100, 'terradreta');
-        this.game.physics.arcade.enable(this.terradreta);
-        this.terradreta.body.immovable = true;
-    };
-    mainState.prototype.createbaldes = function () {
-        this.baldes = this.add.group();
-        this.baldes.enableBody = true;
-        this.baldes.physicsBodyType = Phaser.Physics.ARCADE;
-        for (var x = 0; x < 4; x++) {
-            for (var y = 0; y < 5; y++) {
-                if (x == 1 && y == 2 || x == 2 && y == 0 || x == 2 && y == 4 || x == 3 && y == 1 || x == 3 && y == 2 || x == 3 && y == 3) {
-                    var newElement = new Balda(this.game, y * this.ESPAIH, x * this.ESPAIV + 50, 'baldes', 0);
-                    this.baldes.add(newElement);
-                }
-            }
+    /**
+     * Comprova si la balda està dispobible
+     * @param balda id de la balda
+     * @returns {boolean} false si està disponible true si esta ocupada.
+     */
+    mainState.prototype.comprovarBaldes = function (balda) {
+        var resposta = false;
+        if (balda == 1 && !this.balda1) {
+            this.balda1 = true;
         }
-    };
-    mainState.prototype.create = function () {
-        _super.prototype.create.call(this);
-        this.game.stage.backgroundColor = "#74538f";
-        this.configWorld();
-        this.createbaldes();
-        this.configGat();
-        // Cogemos los cursores para gestionar la entrada
-        this.cursor = this.game.input.keyboard.createCursorKeys();
-        this.tiempo = this.game.add.text(20, 10, "Tiempo : " + this.CONTADORTIEMPO, {
-            font: "25px Fixedsys",
-            fill: "#fff",
-            align: "center"
-        });
-        this.game.time.events.loop(Phaser.Timer.SECOND, this.temporitzadorPartida, this);
-        this.game.time.events.loop(Phaser.Timer.SECOND, this.tempsObjectes, this);
-        this.configObjectes();
-    };
-    mainState.prototype.tirarObjectes = function (gat, objecte) {
-        console.log("entra");
-        var num = objecte.balda;
-        objecte.kill();
-        this.alliberarBlada(num);
-    };
-    mainState.prototype.update = function () {
-        _super.prototype.update.call(this);
-        this.game.physics.arcade.collide(this.gat, this.terradreta);
-        this.game.physics.arcade.collide(this.gat, this.terraesquerra);
-        this.game.physics.arcade.collide(this.baldes, this.gat);
-        this.game.physics.arcade.collide(this.gat, this.objectes, this.tirarObjectes, null, this);
-        this.movePlayer();
-        if (this.contadorObjectes == 1) {
-            this.crearObjectes();
-            this.contadorObjectes = 0;
+        else if (balda == 2 && !this.balda2) {
+            this.balda2 = true;
         }
-        if (this.CONTADORTIEMPO == 0) {
-            this.tiempo.setText("Holiiii");
+        else if (balda == 3 && this.balda3) {
+            this.balda3 = true;
         }
+        else if (balda == 4 && this.balda4) {
+            this.balda4 = true;
+        }
+        else if (balda == 5 && this.balda5) {
+            this.balda5 = true;
+        }
+        else if (balda == 6 && this.balda6) {
+            this.balda4 = true;
+        }
+        else if (balda == 7 && this.balda7) {
+            this.balda7 = true;
+        }
+        else if (balda == 8 && this.balda8) {
+            this.balda8 = true;
+        }
+        else {
+            resposta = true;
+        }
+        return resposta;
     };
+    /**
+     * Possa a true la balda que l'hi introduïm
+     * @param balda número de la balda
+     */
     mainState.prototype.alliberarBlada = function (balda) {
         if (balda == 1) {
             this.balda1 = true;
@@ -248,6 +278,9 @@ var mainState = (function (_super) {
             this.balda8 = true;
         }
     };
+    /***
+     * Metode per moure al jugador (gat)
+     */
     mainState.prototype.movePlayer = function () {
         // Si pulsamos el cursor izquierdo
         if (this.cursor.left.isDown) {
@@ -270,12 +303,26 @@ var mainState = (function (_super) {
         if (this.CONTADORTIEMPO == 0) {
         }
     };
-    mainState.prototype.temporitzadorPartida = function () {
-        this.CONTADORTIEMPO--;
-        this.tiempo.setText("Tiempo : " + this.CONTADORTIEMPO);
-    };
+    /**
+     * Contador de temps per saber quan han de sortir cada un dels objectes
+     */
     mainState.prototype.tempsObjectes = function () {
         this.contadorObjectes++;
+    };
+    mainState.prototype.update = function () {
+        _super.prototype.update.call(this);
+        this.game.physics.arcade.collide(this.gat, this.terradreta);
+        this.game.physics.arcade.collide(this.gat, this.terraesquerra);
+        this.game.physics.arcade.collide(this.baldes, this.gat);
+        this.game.physics.arcade.collide(this.gat, this.objectes, this.tirarObjectes, null, this);
+        this.movePlayer();
+        if (this.contadorObjectes == 1) {
+            this.crearObjectes();
+            this.contadorObjectes = 0;
+        }
+        if (this.CONTADORTIEMPO == 0) {
+            this.tiempo.setText("Holiiii");
+        }
     };
     return mainState;
 })(Phaser.State);
--- main.ts
+++ main.ts
@@ -16,9 +16,7 @@ class mainState extends Phaser.State {
     tiempo:Phaser.Text;
     CONTADORTIEMPO = 30;
     contadorObjectes = 0;
-
     puntuacio = 0;
-
     balda1 = false;
     balda2 = false;
     balda3 = false;
@@ -45,12 +43,133 @@ class mainState extends Phaser.State {
 
 
     }
+
+    /**
+     * Metodes que utilitzaré al create.
+     */
+    /**
+     * Metode configObjectes serveix per congigurar el group d'objectes.
+     */
     configObjectes(){
         this.objectes = this.add.group();
         this.objectes.enableBody = true;
         this.objectes.physicsBodyType = Phaser.Physics.ARCADE;
     }
 
+    /**
+     * Metode configGat serveix per configurar el Sprite Gat
+     */
+    configGat() {
+        this.gat = this.game.add.sprite(
+            this.game.world.centerX,
+            this.game.world.height - 83,
+            'gat'
+        );
+        // Cambiamos el "anchor" del jugador
+        this.gat.anchor.setTo(0.5, 0.5);
+        //Afegueixo fisica al gat.
+        this.game.physics.arcade.enable(this.gat);
+        // que colisione contra las paredes.
+        this.gat.body.collideWorldBounds = true;
+        // Agregamos gravedad al jugador
+        this.gat.body.gravity.y = 500;
+
+    }
+
+    /**
+     * Metode configWorld serveix per configurar el mon
+     */
+    configWorld() {
+        this.game.stage.backgroundColor = "#74538f";
+        this.terraesquerra = this.game.add.sprite(
+            this.game.world.width - 798,
+            this.game.world.height - 100,
+            'terraesquerra'
+        );
+        this.game.physics.arcade.enable(this.terraesquerra);
+        this.terraesquerra.body.immovable = true;
+
+
+        this.baldainici = this.game.add.sprite(
+            this.game.world.width - 565,
+            this.game.world.height - 35,
+            'baldainici'
+        );
+        this.game.physics.arcade.enable(this.baldainici);
+        this.baldainici.body.immovable = true;
+
+        this.terradreta = this.game.add.sprite(
+            this.game.world.width - 250,
+            this.game.world.height - 100,
+            'terradreta'
+        )
+        this.game.physics.arcade.enable(this.terradreta);
+        this.terradreta.body.immovable = true;
+        this.tiempo = this.game.add.text(20, 10, "Tiempo : " + this.CONTADORTIEMPO, {
+            font: "25px Fixedsys",
+            fill: "#fff",
+            align: "center"
+        });
+        this.game.time.events.loop(Phaser.Timer.SECOND, this.temporitzadorPartida, this);
+        this.game.time.events.loop(Phaser.Timer.SECOND, this.tempsObjectes, this);
+        // Cogemos los cursores para gestionar la entrada
+        this.cursor = this.game.input.keyboard.createCursorKeys();
+    }
+
+    /**
+     * Contador de la partida.
+     */
+    temporitzadorPartida() {
+
+        this.CONTADORTIEMPO--;
+        this.tiempo.setText("Tiempo : " + this.CONTADORTIEMPO);
+    }
+    /**
+     * Metode configbaldes serveix per col·locar les baldes a la posició que l'hi toca
+     */
+    configbaldes() {
+
+        this.baldes = this.add.group();
+        this.baldes.enableBody = true;
+        this.baldes.physicsBodyType = Phaser.Physics.ARCADE;
+        for (var x = 0; x < 4; x++) {
+            for (var y = 0; y < 5; y++) {
+                if (x == 1 && y == 2 || x == 2 && y == 0 || x == 2 && y == 4 || x == 3 && y == 1 || x == 3 && y == 2 || x == 3 && y == 3) {
+                    var newElement = new Balda(this.game, y * this.ESPAIH, x * this.ESPAIV + 50, 'baldes', 0);
+                    this.baldes.add(newElement);
+                }
+            }
+        }
+    }
+
+    create():void {
+
+        super.create();
+        this.configWorld();
+        this.configbaldes();
+        this.configGat();
+        this.configObjectes();
+    }
+
+    /**
+     * Metodes utilitzats al update
+     */
+    /**
+     * Metode que al chocar amb un obecte desapareix.
+     * @param gat - Sprite gat (jugador)
+     * @param objecte - Objectes que hi ha a les baldes
+     */
+    tirarObjectes(gat:Phaser.Sprite, objecte:Objecte) {
+        var num = objecte.balda;
+        objecte.kill();
+        this.alliberarBlada(num);
+
+    }
+
+    /**
+     *Metode que retorna un tipus de objecte al atzar.
+     * @returns string tipus d'objecte
+     */
     tipusObjecte():string {
         var tipus = Math.floor((Math.random() * 6) + 1);
         var object;
@@ -67,30 +186,10 @@ class mainState extends Phaser.State {
         }
         return object;
     }
-    comprovarBaldes(balda:number):boolean{
-        var resposta = false;
-        if(balda==1 && !this.balda1){
-            this.balda1=true;
-        }else if(balda==2 && !this.balda2){
-            this.balda2=true;
-        }else if(balda==3 &&  this.balda3){
-            this.balda3=true;
-        }else if(balda==4 && this.balda4){
-            this.balda4=true;
-        }else if(balda==5 && this.balda5){
-            this.balda5=true;
-        }else if(balda==6 && this.balda6){
-            this.balda4=true;
-        }else if(balda==7 && this.balda7){
-            this.balda7=true;
-        }else if(balda==8 && this.balda8){
-            this.balda8=true;
-        } else {
-            resposta = true;
-        }
 
-        return resposta;
-    }
+    /**
+     * Metode que crea objectes al atzar en una posició random
+     */
     crearObjectes() {
 
         var posiciones:Point[] = [
@@ -147,112 +246,39 @@ class mainState extends Phaser.State {
         }
     }
 
-    configGat() {
-        this.gat = this.game.add.sprite(
-            this.game.world.centerX,
-            this.game.world.height - 83,
-            'gat'
-        );
-        // Cambiamos el "anchor" del jugador
-        this.gat.anchor.setTo(0.5, 0.5);
-        //Afegueixo fisica al gat.
-        this.game.physics.arcade.enable(this.gat);
-        // que colisione contra las paredes.
-        this.gat.body.collideWorldBounds = true;
-        // Agregamos gravedad al jugador
-        this.gat.body.gravity.y = 500;
-
-    }
-
-    configWorld() {
-        this.terraesquerra = this.game.add.sprite(
-            this.game.world.width - 798,
-            this.game.world.height - 100,
-            'terraesquerra'
-        );
-        this.game.physics.arcade.enable(this.terraesquerra);
-        this.terraesquerra.body.immovable = true;
-
-
-        this.baldainici = this.game.add.sprite(
-            this.game.world.width - 565,
-            this.game.world.height - 35,
-            'baldainici'
-        );
-        this.game.physics.arcade.enable(this.baldainici);
-        this.baldainici.body.immovable = true;
-
-        this.terradreta = this.game.add.sprite(
-            this.game.world.width - 250,
-            this.game.world.height - 100,
-            'terradreta'
-        )
-        this.game.physics.arcade.enable(this.terradreta);
-        this.terradreta.body.immovable = true;
-
-
-    }
-
-    createbaldes() {
-
-        this.baldes = this.add.group();
-        this.baldes.enableBody = true;
-        this.baldes.physicsBodyType = Phaser.Physics.ARCADE;
-        for (var x = 0; x < 4; x++) {
-            for (var y = 0; y < 5; y++) {
-                if (x == 1 && y == 2 || x == 2 && y == 0 || x == 2 && y == 4 || x == 3 && y == 1 || x == 3 && y == 2 || x == 3 && y == 3) {
-                    var newElement = new Balda(this.game, y * this.ESPAIH, x * this.ESPAIV + 50, 'baldes', 0);
-                    this.baldes.add(newElement);
-                }
-            }
+    /**
+     * Comprova si la balda està dispobible
+     * @param balda id de la balda
+     * @returns {boolean} false si està disponible true si esta ocupada.
+     */
+    comprovarBaldes(balda:number):boolean{
+        var resposta = false;
+        if(balda==1 && !this.balda1){
+            this.balda1=true;
+        }else if(balda==2 && !this.balda2){
+            this.balda2=true;
+        }else if(balda==3 &&  this.balda3){
+            this.balda3=true;
+        }else if(balda==4 && this.balda4){
+            this.balda4=true;
+        }else if(balda==5 && this.balda5){
+            this.balda5=true;
+        }else if(balda==6 && this.balda6){
+            this.balda4=true;
+        }else if(balda==7 && this.balda7){
+            this.balda7=true;
+        }else if(balda==8 && this.balda8){
+            this.balda8=true;
+        } else {
+            resposta = true;
         }
+        return resposta;
     }
 
-    create():void {
-
-        super.create();
-        this.game.stage.backgroundColor = "#74538f";
-        this.configWorld();
-        this.createbaldes();
-        this.configGat();
-        // Cogemos los cursores para gestionar la entrada
-        this.cursor = this.game.input.keyboard.createCursorKeys();
-        this.tiempo = this.game.add.text(20, 10, "Tiempo : " + this.CONTADORTIEMPO, {
-            font: "25px Fixedsys",
-            fill: "#fff",
-            align: "center"
-        });
-        this.game.time.events.loop(Phaser.Timer.SECOND, this.temporitzadorPartida, this);
-        this.game.time.events.loop(Phaser.Timer.SECOND, this.tempsObjectes, this);
-        this.configObjectes();
-    }
-
-    tirarObjectes(gat:Phaser.Sprite, objecte:Objecte) {
-        console.log("entra");
-        var num = objecte.balda;
-        objecte.kill();
-        this.alliberarBlada(num);
-
-    }
-
-    update():void {
-        super.update();
-        this.game.physics.arcade.collide(this.gat, this.terradreta);
-        this.game.physics.arcade.collide(this.gat, this.terraesquerra);
-        this.game.physics.arcade.collide(this.baldes, this.gat);
-        this.game.physics.arcade.collide(this.gat, this.objectes, this.tirarObjectes, null, this);
-        this.movePlayer();
-        if (this.contadorObjectes == 1) {
-            this.crearObjectes();
-            this.contadorObjectes=0;
-        }
-        if(this.CONTADORTIEMPO==0){
-            this.tiempo.setText("Holiiii");
-        }
-
-    }
-
-
+    /**
+     * Possa a true la balda que l'hi introduïm
+     * @param balda número de la balda
+     */
     alliberarBlada(balda:number) {
         if (balda == 1) {
             this.balda1 = true;
@@ -273,6 +299,9 @@ class mainState extends Phaser.State {
         }
     }
 
+    /***
+     * Metode per moure al jugador (gat)
+     */
     movePlayer():void {
         // Si pulsamos el cursor izquierdo
         if (this.cursor.left.isDown) {
@@ -300,15 +329,29 @@ class mainState extends Phaser.State {
 
     }
 
-    temporitzadorPartida() {
-
-        this.CONTADORTIEMPO--;
-        this.tiempo.setText("Tiempo : " + this.CONTADORTIEMPO);
-    }
-
+    /**
+     * Contador de temps per saber quan han de sortir cada un dels objectes
+     */
     tempsObjectes() {
         this.contadorObjectes++;
     }
+    update():void {
+        super.update();
+        this.game.physics.arcade.collide(this.gat, this.terradreta);
+        this.game.physics.arcade.collide(this.gat, this.terraesquerra);
+        this.game.physics.arcade.collide(this.baldes, this.gat);
+        this.game.physics.arcade.collide(this.gat, this.objectes, this.tirarObjectes, null, this);
+        this.movePlayer();
+        if (this.contadorObjectes == 1) {
+            this.crearObjectes();
+            this.contadorObjectes=0;
+        }
+        if(this.CONTADORTIEMPO==0){
+            this.tiempo.setText("Holiiii");
+        }
+
+    }
+
 
 }
 class Objecte extends Phaser.Sprite {
